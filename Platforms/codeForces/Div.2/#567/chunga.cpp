8949438db6932fd5e4be2eb1b4915ed9e22d857b
manointern <mano_intern@aspirehive.com>
2019-07-04 21:56:07 +0530

    Update.

--- Platforms/codeForces/Div.2/#567/chunga.cpp
+++ Platforms/codeForces/Div.2/#567/chunga.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
+#include<cstdint> 
 #define ll long long int
+#define mod 1000000007
 using namespace std;
 
 int main()
 {
-    ll x, y, z, sum;
+    int64_t  x, y, z, sum;
 
     cin >> x >> y >> z;
 
     sum = 0;
 
-    ll remSum = 0;
+    int64_t  remSum = 0;
 
     sum += x / z;
     sum += y / z;
 
     remSum = ((x % z) + (y % z)) / z;
-    sum += remSum;
+    sum += remSum % mod;
 
-    ll minim = min((x % z), (y % z));
+    int64_t  minim = min((x % z), (y % z));
 
-    cout << sum << " " << minim << '\n';
+    cout << (sum % mod) << " " << minim << '\n';
 }
