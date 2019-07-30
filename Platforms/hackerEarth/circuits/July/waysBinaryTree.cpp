1be536e51d2cde4d54f46a641cd6deca2ae4da0f
Mano Sriram <mano.sriram0@gmail.com>
2019-07-30 23:58:40 +0530

    Ways BT.

---
+++ Platforms/hackerEarth/circuits/July/waysBinaryTree.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#define trace(x) cerr << #x << " " << x << '\n';
+#define ld long long
+#define disp(n) for (auto t : n)
+#define forn(t, n) for (ld t = 0; t < n; t++)
+#define pb push_back
+#define mod 1000000007
+#define c_sort(a) sort(a, a + sizeof(a) / sizeof(a[0]))
+#define NL cout << '\n';
+#define INF (int)1e9
+using namespace std;
+typedef vector<int> vI;
+
+int main()
+{
+    cin.sync_with_stdio(false);
+    cin.tie(NULL);
+    ld T;
+
+    return 0;
+}
