b22b091ec36b3e2717aeabcfff9b462229ada49c
Mano Sriram <mano.sriram0@gmail.com>
2019-10-25 03:47:04 +0530

    Update Basic Header File.

--- Bit-Masking/Basic.hpp
+++ Bit-Masking/Basic.hpp
@@ -11,7 +11,7 @@ int leftShift(int a) { return (a << 1); }
 
 int COMP(int a) { return ~a; }
 
-int EXOR(int a, int b) { return (a ^ b); }
+int XOR(int a, int b) { return (a ^ b); }
 
 int OR(int a, int b) { return (a | b); }
 
